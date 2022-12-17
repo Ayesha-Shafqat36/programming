31e213cf1bcb531037dec9360a0df7582626ca25
Ayesha Shafqat <ayeshafqat36@gmail.com>
2022-12-17 20:42:38 +0500

    programming

---
+++ example.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+cout << "My name is Ayesha shafqat" << endl << "My roll no is 11385";
+
+}
---
+++ projectname.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+cout << "**********************************************" << endl;
+cout << "*                                            *" << endl;
+cout << "*                                            *" << endl;
+cout << "*           AYESHAFQAT S-HOUSE               *" << endl;
+cout << "*                                            *" << endl;
+cout << "*                                            *" << endl;
+cout << "**********************************************" << endl;
+
+}
